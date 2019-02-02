4d1d5c3965b3fa4b5069f6ffd74935b7732b028b
KordianD <kordiandrozd@gmail.com>
2019-02-02 15:18:20 +0100

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='interactive-clang-static-analysis',
+    packages=['interactive-clang-static-analysis'],
+    version='0.01',
+    description='interactive-clang-static-analysis',
+    author='KordianD',
+    author_email='not_valid@gmail.com',
+    url='https://github.com/KordianD/interactive-clang-static-analysis',
+    keywords=['clang', 'static analysis', 'clang'],
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License'],
+    long_description_content_type='text/markdown'
+)
