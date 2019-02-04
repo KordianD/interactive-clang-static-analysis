1a93703f5a072141ae793cba2119c14c3a6f6f78
KordianD <kordiandrozd@gmail.com>
2019-02-04 18:12:31 +0100

    Created clang_warning schema

---
+++ src/db_schemas/clang_warning.py
@@ -0,0 +1,7 @@
+from mongoengine import Document, StringField, IntField
+
+
+class ClangWarning(Document):
+    check_name = StringField(required=True)
+    path_to_file = StringField(required=True)
+    line_number = IntField(required=True)
