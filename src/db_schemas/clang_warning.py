3dd0df21c5234ded7b91ade186bf56f1ea75d7b0
KordianD <kordiandrozd@gmail.com>
2019-02-04 19:25:44 +0100

    Schema updated

--- src/db_schemas/clang_warning.py
+++ src/db_schemas/clang_warning.py
@@ -1,7 +1,9 @@
-from mongoengine import Document, StringField, IntField
+from mongoengine import Document, StringField, IntField, DateTimeField
 
 
 class ClangWarning(Document):
     check_name = StringField(required=True)
     path_to_file = StringField(required=True)
     line_number = IntField(required=True)
+    owner = StringField(required=True)
+    date = DateTimeField(required=True)
