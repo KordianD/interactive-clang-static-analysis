baa82e39e828238e62974ce127c393cc004cda2d
KordianD <kordiandrozd@gmail.com>
2019-02-04 17:48:01 +0100

    Added more checks to unit test

--- test/parser/test_get_clang_warnings.py
+++ test/parser/test_get_clang_warnings.py
@@ -6,6 +6,6 @@ clang_warning = [line.rstrip('\n') for line in open(
 
 def test_get_clang_warnings():
     returned_clang_warnings = get_clang_warnings(clang_warning)
-    print('RETURNED')
-    print(returned_clang_warnings[0].check_name)
     assert returned_clang_warnings[0].check_name == 'modernize-use-override'
+    assert returned_clang_warnings[0].path_to_file == '/home/test/CLionProjects/untitled/main.cpp'
+    assert returned_clang_warnings[0].line_number == 8
